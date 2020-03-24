8e199a522075400acd8c23664313504a0f3783ca
zizdog <i@zizdog.com>
2020-03-24 19:03:31 +0800

    fix

--- ic/ajax/dlist.php
+++ ic/ajax/dlist.php
@@ -2,7 +2,7 @@
 $q = isset($_GET["q"]) ? intval($_GET["q"]) : ''; 
 
 //$con=mysqli_connect("localhost","数据库用户名","数据库密码","数据库名"); 
-$con=mysqli_connect("localhost","x","x","x");
+$con=mysqli_connect("localhost","db_user","db_password","db_name");
 if (!$con)
 {
     die('数据获取失败:请检查"ic/ajax/dlist.php"中数据库配置 ' . mysqli_error($con));
